4a3364654f2dd8365bf5102a24505de32050a79b
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-04-23 16:11:12 +0200

    first commit

---
+++ src/main/java/com/codecool/CoolNote/CoolNoteApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.CoolNote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoolNoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoolNoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/CoolNote/CoolNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.CoolNote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoolNoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
