f4a2241663f52bee1c45cade030bfe46911dfd58
Chomentowski Sebastian <chomentowski.s@gmail.com>
2022-05-08 10:45:19 +0200

    adding hibernate

--- src/main/java/com/codecool/CoolNote/Note.java
+++ src/main/java/com/codecool/CoolNote/Note.java
@@ -1,5 +1,14 @@
 package com.codecool.CoolNote;
+
+import javax.persistence.*;
+
+@Entity
 public class Note {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private long id;
+
     private String message;
 
     public Note(String message) {
@@ -11,5 +20,17 @@ public class Note {
 
     public String getMessage() {
         return message;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
--- src/main/java/com/codecool/CoolNote/NoteRepository.java
+++ src/main/java/com/codecool/CoolNote/NoteRepository.java
@@ -1,25 +1,9 @@
 package com.codecool.CoolNote;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository
-public class NoteRepository {
-    private static final List<Note> notes = new ArrayList<>();
-
-
-    @PostConstruct
-    public void init() {
-        notes.add(new Note("Ala ma kota"));
-        notes.add(new Note("dupa"));
-        notes.add(new Note("Kot ma ale"));
-    }
-
-    public List<Note> findAll() {
-        return notes;
-    }
-
+public interface NoteRepository extends JpaRepository<Note, Long> {
 }
